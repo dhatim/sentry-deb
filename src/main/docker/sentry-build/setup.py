5e8b993ccb6642cdb9473c062f8c914477881e91
Olivier Chédru <ochedru@dhatim.com>
2017-03-07 09:04:51 +0100

    Bump version to 8.14.1 to fix migration issue

--- src/main/docker/sentry-build/setup.py
+++ src/main/docker/sentry-build/setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__' and sys.argv[1] == 'test':
 
 setup(
     name='sentry',
-    version='8.14.0',
+    version='8.14.1',
     author='Dhatim',
     author_email='dev-oss@dhatim.com',
     packages=[],
