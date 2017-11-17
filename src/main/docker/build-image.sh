833a606b068751be6754b928fd50eaa5acd9c025
Olivier Chédru <ochedru@dhatim.com>
2017-11-17 10:49:50 +0100

    Add missing libpng-dev

--- src/main/docker/build-image.sh
+++ src/main/docker/build-image.sh
@@ -8,7 +8,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python /tmp/get-pip.py
 rm /tmp/get-pip.py
 
-apt-get install --assume-yes --no-install-recommends python-dev dh-virtualenv gcc-multilib clang cmake libxml2-dev libxslt-dev libpq-dev libffi-dev libjpeg62-turbo-dev zlib1g-dev libssl-dev build-essential devscripts equivs git sudo
+apt-get install --assume-yes --no-install-recommends python-dev dh-virtualenv gcc-multilib clang cmake libxml2-dev libxslt-dev libpq-dev libffi-dev libjpeg62-turbo-dev libpng-dev zlib1g-dev libssl-dev build-essential devscripts equivs git sudo
 
 # install build dependencies for sentry-deb
 cd /wd/pkg
